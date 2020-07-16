3f82f7b1a9a81ba3478ff79b11d78ab000c66a77
Walid Lezzar <walid.lezzar@gmail.com>
2020-07-16 12:58:12 +0200

    bump Formula to 0.5.0

--- Formula/jsql.rb
+++ Formula/jsql.rb
@@ -2,10 +2,10 @@ class Jsql < Formula
   desc "Execute SQL on json data"
   homepage "https://github.com/wlezzar/jsql"
   bottle :unneeded
-  version "0.4.0"
+  version "0.5.0"
   
-  url "https://github.com/wlezzar/jsql/releases/download/0.4.0/jsql.zip"
-  sha256 "79d26f8693f9896cc7cd26e959a63a17c02b9c8f7f9914d3cb4cb8624b6b2cf5"
+  url "https://github.com/wlezzar/jsql/releases/download/0.5.0/jsql.zip"
+  sha256 "1d206b22919cf8bfd6eef0202ea384622a4ca93fc2a0de5404ac1237558d4a0c"
 
   def install
     bin.install "bin/jsql"
